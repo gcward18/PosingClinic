e8185515b081c44d25ceb51c2712797a5f3f131d
George C. Ward <gcward18@gmail.com>
2025-06-24 23:20:03 -0400

    Refactor code structure for improved readability and maintainability

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -12,13 +12,13 @@ export default function Sidebar() {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
                 </svg>
             </button>
-            <div className={`fixed top-0 left-0 h-full bg-gray-100  w-64 h-full bg-white border-r border-gray-300 ${open ? "translate-x-0" : "-translate-x-full"}`}>
+            <div className={`fixed top-0 left-0 h-full bg-gray-100  w-64 h-full bg-white border-r border-gray-300 ${open ? "translate-x-0" : "-translate-x-full"}  z-1`}>
                 <button onClick={() => setOpen(false)} className="close-button flex top-0 left-0 m-2 p-2 bg-blue-500 rounded-sm text-white cursor-pointer">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
                     </svg>
                 </button>
-                <ul class="p-4 text-xl gap-3 flex flex-col">
+                <ul className="p-4 text-xl gap-3 flex flex-col">
                     <li className="py-2 ">
                         <a href="/" className="flex items-center gap-2 hover:bg-gray-900/10 hover:rounded-sm p-4 hover:scale-105 transition">
                         <i className="fas fa-house"></i> Home
@@ -30,8 +30,18 @@ export default function Sidebar() {
                         </a>
                     </li>
                     <li className="py-2">
+                        <a href="/divisions" className="flex items-center gap-2 hover:bg-gray-900/10 hover:rounded-sm p-4 hover:scale-105 transition">
+                        <i className="fas fa-list"></i> View Divisions
+                        </a>
+                    </li>
+                    <li className="py-2">
                         <a href="/competitor" className="flex items-center gap-2 hover:bg-gray-900/10 hover:rounded-sm p-4 hover:scale-105 transition">
                         <i className="fas fa-user"></i> Add Competitor
+                        </a>
+                    </li>
+                    <li className="py-2">
+                        <a href="/competitors" className="flex items-center gap-2 hover:bg-gray-900/10 hover:rounded-sm p-4 hover:scale-105 transition">
+                        <i className="fas fa-list"></i> View Competitors
                         </a>
                     </li>
                     <li className="py-2">
@@ -40,13 +50,28 @@ export default function Sidebar() {
                         </a>
                     </li>
                     <li className="py-2">
+                        <a href="/competitions" className="flex items-center gap-2 hover:bg-gray-900/10 hover:rounded-sm p-4 hover:scale-105 transition">
+                        <i className="fas fa-list"></i> View Competitions
+                        </a>
+                    </li>
+                    <li className="py-2">
                         <a href="/judge" className="flex items-center gap-2 hover:bg-gray-900/10 hover:rounded-sm p-4 hover:scale-105 transition">
                         <i className="fas fa-gavel"></i> Add Judge
                         </a>
                     </li>
                     <li className="py-2">
+                        <a href="/judges" className="flex items-center gap-2 hover:bg-gray-900/10 hover:rounded-sm p-4 hover:scale-105 transition">
+                        <i className="fas fa-list"></i> View Judges
+                        </a>
+                    </li>
+                    <li className="py-2">
                         <a href="/user" className="flex items-center gap-2 hover:bg-gray-900/10 hover:rounded-sm p-4 hover:scale-105 transition">
                         <i className="fas fa-users"></i> Add User
+                        </a>
+                    </li>
+                    <li className="py-2">
+                        <a href="/users" className="flex items-center gap-2 hover:bg-gray-900/10 hover:rounded-sm p-4 hover:scale-105 transition">
+                        <i className="fas fa-list"></i> View Users
                         </a>
                     </li>
                 </ul>
--- frontend/src/pages/users/UsersListPage.tsx
+++ frontend/src/pages/users/UsersListPage.tsx
@@ -2,14 +2,13 @@ import React from "react";
 import { GenericProvider } from "../../components/contexts/GenericContext";
 import GenericTable from "../../components/tables/GenericTable";
 
-export default function JudgesListPage() {
+export default function UsersListPage() {
     // @ts-ignore
     return (
         <div className="flex items-center justify-center  ">
             <div className="p-6 rounded shadow-md w-full max-w-sm">
-                <h2 className="text-2xl font-bold mb-4">View Judge</h2>
-
-                <GenericProvider dataType="judges">
+                <h2 className="text-2xl font-bold mb-4">View Users</h2>
+                <GenericProvider dataType="users">
                     <GenericTable />
                 </GenericProvider>
             </div>
